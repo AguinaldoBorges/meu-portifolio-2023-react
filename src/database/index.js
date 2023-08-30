16993a8ecd7d8533a14116c8f952c1fd8a9d9b87
AguinaldoBorges <tiaguinaldo2016@gmail.com>
2023-08-30 20:43:16 -0300

    Agualizadas tecnologias

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const projetos = [
     {
         title: "Este Portfólio",
-        technologies: "React, CSS e Bootstrap",
+        technologies: "React, CSS e Bootstrap, Git/Github - Versionamento",
         description:
             "Nesse projeto desenvolvi este portfolio usando componentes. Possui sistema de filtro para exibir os projetos de acordo com a tecnologia selecionada. Ao tocar na imagem de um projeto ela começa a se mexer, mostrando o projeto em uso. Na sessão de skills é exibida uma descrição ao selecionar cada uma. Adicionei um botão fluruante para rolar a página ao topo a qualquer momento durante a navegação. O site está responsivo, adequando-se a tablets dispositivos moveis.",
         links: [
@@ -22,7 +22,7 @@ const projetos = [
     },
     {
         title: "Guina Cursos - Front-End",
-        technologies: "React, Css e Bootstrap",
+        technologies: "React, Css e Bootstrap, Git/Github - Versionamento",
         description:
             "Para construir o 'Guina Cursos', escolhi utilizar tecnologias modernas para o desenvolvimento web, incluindo React para criar interfaces dinâmicas e reativas, React Router Dom para gerenciar as rotas da aplicação, useState para o gerenciamento de estados nos componentes, além de utilizar o Bootstrap para estilização responsiva.",
         links: [{ label: "Código", url: "https://github.com/AguinaldoBorges/guina-cursos" }],
@@ -46,7 +46,7 @@ const projetos = [
     },
     {
         title: "Gina Hernandez",
-        technologies: "Html5, CSS3, Bootstrap, Javascript",
+        technologies: "Html5, CSS3, Bootstrap, Javascript, Git/Github - Versionamento",
         description:
             "Nesse projeto usei HTML, CSS e JavaScript, juntamente com bibliotecas como Bootstrap e jQuery UI. Essas ferramentas me permitiram construir uma interface responsiva, com elementos interativos e visualmente atrativos. Explorei as funcionalidades do Javascript e implementei um sistema de eventos que atualiza a imagem principal de acordo com a seleção.Utilizei a biblioteca jQuery UI Datepicker para criar um calendário que permitisse aos usuários visualizar datas importantes. Usei o Bootstrap para criar um layout responsivo, mas ainda precisei ajustar manualmente alguns elementos para garantir uma experiência agradável em todas as telas.",
         links: [
@@ -57,7 +57,7 @@ const projetos = [
     },
     {
         title: "SP CALC",
-        technologies: "Html5, CSS3, GuinaStrap e Javascript",
+        technologies: "Html5, CSS3, GuinaStrap e Javascript, Git/Github - Versionamento",
         description:
             "Nesse sistema, criei um layout responsivo usando HTML e CSS. Usei suavização no scroll, tags semanticas, Javascript para criar a calculadora e lógica para previnir comportamentos do usuário durante sua execução. Também usei o GuinaStrap, que é minha própria biblioteca Css, criada para otimizar o desenvolvimento front-end. O SP CALC é um sistema desenvolvido para fins de estudos.",
         links: [
@@ -68,7 +68,7 @@ const projetos = [
     },
     {
         title: "Reciclagem",
-        technologies: "Vue.Js, Css e Bootstrap",
+        technologies: "Vue.Js, Css e Bootstrap, Git/Github - Versionamento",
         description:
             "Usando componentização, menu fixo e um botão de voltar ao topo para melhor experiencia do usuário, esse projeto trouxe o desafio de comunicar o tema de reciclagem de maneira lúdica. Ele foi desenvolvido como um bonus e usado na feira de ciências de acadêmicos da Faculdade Flamingo. Hoje está integrada na rede interna da faculdade, disponível para cosulta dos alunos.",
         links: [
@@ -79,7 +79,7 @@ const projetos = [
     },
     {
         title: "Buscador Patomon",
-        technologies: "React, Css e Bootstrap, API",
+        technologies: "React, Css e Bootstrap, API, Git/Github - Versionamento",
         description:
             "O desafio aqui foi consumir uma api, criar um sistema de busca e exibir o pokemon pesquisado. Para quem joga os games, criei a área golpes, onde o usuário pode selecionar os ataques compatíveis com o pokemon escolhido para bolar suas extratégias.",
         links: [
